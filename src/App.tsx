2aff1c1e284778a55de19888d52ca0e8643475cd
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2026-04-02 15:18:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -793,7 +793,7 @@ export default function App() {
             }}
           />
 
-  <div className="pointer-events-none absolute bottom-24 left-1/2 z-[9] w-full max-w-6xl -translate-x-1/2 px-3 sm:px-6">
+  <div className="pointer-events-none absolute bottom-28 left-1/2 z-[9] w-full max-w-6xl -translate-x-1/2 px-3 sm:px-6">
   <div className="grid grid-cols-3 gap-2 sm:gap-7 md:gap-9">
     <motion.div
       initial={{ opacity: 0, y: 12 }}
